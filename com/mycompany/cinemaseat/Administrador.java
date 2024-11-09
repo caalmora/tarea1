6a2e75e2806d595c5a4c831530351e6fd55f105a
caalmora <caalmora@espol.edu.ec>
2024-11-08 22:35:58 -0500

    Camila
    Paso 1

---
+++ com/mycompany/cinemaseat/Administrador.java
@@ -0,0 +1,25 @@
+package com.mycompany.cinemaseat;
+
+/**
+ *
+ * @author User
+ */
+public class Administrador extends Usuario {
+    public Administrador(String nombre, String correo, String tipoUsuario) {
+        super(nombre, correo, tipoUsuario);
+    }
+
+    public void gestionarPrecios(TipoAsiento tipoAsiento, double precio) {
+        System.out.println("Actualizando el precio para " + tipoAsiento.getClass().getSimpleName() + " a " + precio);
+    }
+
+    public void agregarSala(Cine cine, Sala sala) {
+        cine.agregarSala(sala);
+        System.out.println("Sala " + sala + " agregada al cine " + cine);
+    }
+
+    public void agregarFuncion(Sala sala, Funcion funcion) {
+        sala.agregarFuncion(funcion);
+        System.out.println("Función agregada a la sala " + sala);
+    }
+}
