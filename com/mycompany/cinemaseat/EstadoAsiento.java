aca580450f8d28a528fac9b4245b1e5a63df1843
caalmora <caalmora@espol.edu.ec>
2024-11-08 23:13:59 -0500

    Camila
    3

--- com/mycompany/cinemaseat/EstadoAsiento.java
+++ com/mycompany/cinemaseat/EstadoAsiento.java
@@ -1,3 +1,6 @@
 public enum EstadoAsiento {
+package com.mycompany.cinemaseat;
 
+public enum EstadoAsiento {
+    DISPONIBLE, RESERVADO, EN_PROCESO_COMPRA
 }
--- com/mycompany/cinemaseat/EstadoReserva.java
+++ com/mycompany/cinemaseat/EstadoReserva.java
@@ -2,4 +2,5 @@ package com.mycompany.cinemaseat;
 
 public enum EstadoReserva {
 
+    EN_PROCESO, CONFIRMADA, CANCELADA
 }
